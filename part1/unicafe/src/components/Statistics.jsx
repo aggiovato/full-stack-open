8052847bec71ddcd0e1ad94d7a98d58eadd0923b
Aggiovato <aggiovato@gmail.com>
2024-11-25 01:22:06 +0100

    Commit 1.9: Add conditional rendering to statistics component, if there's 0 feedback it shows 'No feedback given' message.

--- part1/unicafe/src/components/Statistics.jsx
+++ part1/unicafe/src/components/Statistics.jsx
@@ -2,17 +2,26 @@ import { all, average, positive } from "../utils/helpers";
 
 const Statistics = ({ stats }) => {
   const { good, neutral, bad } = stats;
+
+  const all_fb = all(good, neutral, bad);
+  const average_fb = average(good, neutral, bad).toFixed(2);
+  const positive_fb = positive(good, neutral, bad).toFixed(2);
+
   return (
     <>
       <h1>Statistics</h1>
-      <ul>
-        <li>Good: {good}</li>
-        <li>Neutral: {neutral}</li>
-        <li>Bad: {bad}</li>
-        <li>All: {all(good, neutral, bad)}</li>
-        <li>Average: {average(good, neutral, bad).toFixed(2)}</li>
-        <li>Positive: {positive(good, neutral, bad).toFixed(2)} %</li>
-      </ul>
+      {!all_fb ? (
+        <p>No feedbacks given</p>
+      ) : (
+        <ul>
+          <li>Good: {good}</li>
+          <li>Neutral: {neutral}</li>
+          <li>Bad: {bad}</li>
+          <li>All: {all_fb}</li>
+          <li>Average: {average_fb}</li>
+          <li>Positive: {positive_fb} %</li>
+        </ul>
+      )}
     </>
   );
 };
