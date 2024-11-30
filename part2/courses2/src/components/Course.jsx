27b3f70643a34d07c6595b677b41ea332bcf59b6
Aggiovato <aggiovato@gmail.com>
2024-11-30 20:07:34 +0100

    Commit 2.2 and 2.3*: Create Total component to display the total amount of exercises for each course. Use 'reduce' higher-order function to do so.

--- part2/courses2/src/components/Course.jsx
+++ part2/courses2/src/components/Course.jsx
@@ -12,12 +12,21 @@ const Content = ({ parts }) => {
 
 const Part = ({ name, exercises }) => <p>{`${name} ${exercises}`}</p>;
 
+const Total = ({ parts }) => {
+  const total_exercises = parts.reduce(
+    (total, part) => total + part.exercises,
+    0
+  );
+  return <p>Total of {total_exercises} exercises</p>;
+};
+
 const Course = ({ course }) => {
   const { course_name, parts } = course;
   return (
     <>
       <Header course_name={course_name} />
       <Content parts={parts} />
+      <Total parts={parts} />
     </>
   );
 };
