8dc3ab13a4b99e2221dcaa81d5417370b308a4e0
Aggiovato <aggiovato@gmail.com>
2024-11-30 19:43:16 +0100

    Commit 2.1: Add basic structure for displaying information about courses, create Course, Header, Content and Part components for this purpose: - App   - Course     - Header     - Content       - Part       - Part ...

--- part2/courses2/src/App.jsx
+++ part2/courses2/src/App.jsx
@@ -1,29 +1,8 @@
 import Course from "./components/Course.jsx";
+import course_data from "./utils/data.json";
 
 const App = () => {
-  const course = {
-    id: 1,
-    name: "Half Stack application development",
-    parts: [
-      {
-        name: "Fundamentals of React",
-        exercises: 10,
-        id: 1,
-      },
-      {
-        name: "Using props to pass data",
-        exercises: 7,
-        id: 2,
-      },
-      {
-        name: "State of a component",
-        exercises: 14,
-        id: 3,
-      },
-    ],
-  };
-
-  return <Course course={course} />;
+  return <Course course={course_data} />;
 };
 
 export default App;
--- part2/courses2/src/components/Course.jsx
+++ part2/courses2/src/components/Course.jsx
@@ -1,12 +1,23 @@
+const Header = ({ course_name }) => <h1>{course_name}</h1>;
+
+const Content = ({ parts }) => {
+  return (
+    <div>
+      {parts.map((part) => (
+        <Part key={part.id} name={part.name} exercises={part.exercises} />
+      ))}
+    </div>
+  );
+};
+
+const Part = ({ name, exercises }) => <p>{`${name} ${exercises}`}</p>;
+
 const Course = ({ course }) => {
+  const { course_name, parts } = course;
   return (
     <>
-      <h1>{course.name}</h1>
-      <ul>
-        {course.parts.map((part) => (
-          <li key={part.id}>{part.name}</li>
-        ))}
-      </ul>
+      <Header course_name={course_name} />
+      <Content parts={parts} />
     </>
   );
 };
