9191fb45b41da25890bb1bb0e9983c3a604eb8aa
Aggiovato <aggiovato@gmail.com>
2024-11-30 19:59:53 +0100

    Commit 2.1*: Add some other courses to a new json file data2.json to iterate the array and show more information.

--- part2/courses2/src/App.jsx
+++ part2/courses2/src/App.jsx
@@ -1,8 +1,15 @@
 import Course from "./components/Course.jsx";
-import course_data from "./utils/data.json";
+//import course_data from "./utils/data.json";
+import courses_data from "./utils/data2.json";
 
 const App = () => {
-  return <Course course={course_data} />;
+  return (
+    <>
+      {courses_data.map((course) => (
+        <Course key={course.id} course={course} />
+      ))}
+    </>
+  );
 };
 
 export default App;
