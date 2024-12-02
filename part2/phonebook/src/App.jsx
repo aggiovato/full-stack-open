32acb3bedf5f29e630c134b49f4dcafe6c8ad923
Aggiovato <aggiovato@gmail.com>
2024-12-02 02:34:04 +0100

    Commit 2.10*: Add new structure to App.jsx component.

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -9,7 +9,6 @@ import contactData from "@data";
 
 const App = () => {
   const [contacts, setContacts] = useState(contactData);
-
   const [filteredContacts, setFilteredContacts] = useState(contacts);
 
   const handleListUpdate = (newContacts) => {
@@ -33,7 +32,6 @@ const App = () => {
     <div>
       <GlobalStyle />
       <Filter handleFilter={handleFilter} />
-
       <Form list={contacts} handleList={handleListUpdate} />
       <NumberList list={filteredContacts} />
     </div>
@@ -45,7 +43,6 @@ export default App;
 /**
  * Global styles
  */
-
 const GlobalStyle = createGlobalStyle`
   body {
   font-family: 'Nunito', sans-serif;
@@ -56,24 +53,3 @@ const GlobalStyle = createGlobalStyle`
     box-sizing: border-box;
   }
 `;
-
-/*const AppContainer = styled.div`
-  display: flex;
-  align-items: center;
-  padding: 10px 30px;
-  margin: 0px 300px;
-
-  @media (max-width: 768px) {
-    /* Tablet */
-/*padding: 20px 25px;
-    max-width: 90%;
-  }*/
-
-/*@media (max-width: 480px) {
-    /* Mobile */
-/*flex-direction: column;
-    align-items: stretch;
-    padding: 5px 5px;
-    margin: 0px 0px;
-  }
-`;*/
