c888b445315c64c35511c7505715cb73af6f0787
Aggiovato <aggiovato@gmail.com>
2024-12-17 02:59:20 +0100

    *

---
+++ rest-countries-v2/vite.config.js
@@ -0,0 +1,23 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+      "@components": path.resolve(__dirname, "src/components"),
+      "@styles": path.resolve(__dirname, "src/styles"),
+      "@utils": path.resolve(__dirname, "src/utils"),
+      "@hooks": path.resolve(__dirname, "src/hooks"),
+      "@context": path.resolve(__dirname, "src/context"),
+      "@assets": path.resolve(__dirname, "src/assets"),
+      "@services": path.resolve(__dirname, "src/services"),
+    },
+  },
+});
