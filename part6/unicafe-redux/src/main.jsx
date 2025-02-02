ddca53383b6640024262ee7784df400205823b34
Aggiovato <aggiovato@gmail.com>
2025-02-02 22:13:39 +0100

    Commit 6.2: Add implementation for frontend with the reducer
    Add a simple style file

--- part6/unicafe-redux/src/main.jsx
+++ part6/unicafe-redux/src/main.jsx
@@ -1,36 +1,62 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./styles.css";
 
-import { createStore } from 'redux'
-import reducer from './reducer'
+import { createStore } from "redux";
+import reducer from "./reducer";
 
-const store = createStore(reducer)
+// CREATE STORE
+const store = createStore(reducer);
+
+// DISPATCH ACTIONS
+const good = () => {
+  store.dispatch({
+    type: "GOOD",
+  });
+};
+
+const ok = () => {
+  store.dispatch({
+    type: "OK",
+  });
+};
+
+const bad = () => {
+  store.dispatch({
+    type: "BAD",
+  });
+};
+
+const reset = () => {
+  store.dispatch({
+    type: "RESET",
+  });
+};
 
 const App = () => {
-  const good = () => {
-    store.dispatch({
-      type: 'GOOD'
-    })
-  }
-
   return (
-    <div>
-      <button onClick={good}>good</button> 
-      <button>ok</button> 
-      <button>bad</button>
-      <button>reset stats</button>
-      <div>good {store.getState().good}</div>
-      <div>ok</div>
-      <div>bad</div>
-    </div>
-  )
-}
+    <>
+      <div className="btn-panel">
+        <button onClick={good}>good</button>
+        <button onClick={ok}>ok</button>
+        <button onClick={bad}>bad</button>
+        <button onClick={reset}>reset stats</button>
+      </div>
 
-const root = ReactDOM.createRoot(document.getElementById('root'))
+      <div className="stats">
+        <div>good: {store.getState().good}</div>
+        <div>ok: {store.getState().ok}</div>
+        <div>bad: {store.getState().bad}</div>
+      </div>
+    </>
+  );
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
 const renderApp = () => {
-  root.render(<App />)
-}
+  root.render(<App />);
+};
 
-renderApp()
-store.subscribe(renderApp)
+renderApp();
+store.subscribe(renderApp);
