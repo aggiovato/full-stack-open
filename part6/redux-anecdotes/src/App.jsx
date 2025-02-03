54b8d7d66c6808406e75f07f0f56bf2cdd8c48fa
Aggiovato <aggiovato@gmail.com>
2025-02-03 03:45:03 +0100

    Commit 6.8: Create a new component 'AnecdoteList' to render all the anecdotes

--- part6/redux-anecdotes/src/App.jsx
+++ part6/redux-anecdotes/src/App.jsx
@@ -1,29 +1,16 @@
-import { useSelector, useDispatch, shallowEqual } from "react-redux";
 // COMPONENTS
+import AnecdoteList from "./components/AnecdoteList";
 import AnecdoteForm from "./components/AnecdoteForm";
-// REDUX ACTIONS CREATORS
-import { vote } from "./reducers/anecdoteReducer";
 // CSS
 import "./App.css";
 
 /******************************************************************************/
 
 const App = () => {
-  const anecdotes = useSelector((state) => state, shallowEqual);
-  const dispatch = useDispatch();
-
   return (
     <div className="root">
       <h2>Anecdotes</h2>
-      {anecdotes.map((anecdote) => (
-        <div className="anecdote-card" key={anecdote.id}>
-          <div className="anecdote-content">{anecdote.content}</div>
-          <div className="anecdote-votes">
-            has {anecdote.votes}
-            <button onClick={() => dispatch(vote(anecdote.id))}>vote</button>
-          </div>
-        </div>
-      ))}
+      <AnecdoteList />
       <AnecdoteForm />
     </div>
   );
