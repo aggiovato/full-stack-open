7169fbe604b29b806c25aa91d858b38d0a397eb4
Aggiovato <aggiovato@gmail.com>
2025-02-06 22:41:43 +0100

    Commit 6.12: Create Notification componen to render info about other reducers actions

--- part6/redux-anecdotes/src/App.jsx
+++ part6/redux-anecdotes/src/App.jsx
@@ -2,12 +2,14 @@ import styled from "styled-components";
 // COMPONENTS
 import Header from "./components/Header";
 import MainContent from "./components/MainContent";
+import Notification from "./components/Notification";
 
 /******************************************************************************/
 
 const App = () => {
   return (
     <AppContainer>
+      <Notification />
       <Header />
       <MainContent />
     </AppContainer>
