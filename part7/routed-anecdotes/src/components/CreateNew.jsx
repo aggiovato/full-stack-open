388ce81ec1a1904a43aeb13c1e3276b1e83757bd
Aggiovato <aggiovato@gmail.com>
2025-03-30 22:06:37 +0200

    Commit 7.5: Add reset function logic

--- part7/routed-anecdotes/src/components/CreateNew.jsx
+++ part7/routed-anecdotes/src/components/CreateNew.jsx
@@ -1,49 +1,36 @@
 /***** HOOKS *****/
 import useField from "../hooks/useField";
+import formData from "../data/formData.json";
 
 const CreateNew = ({ addNew }) => {
-  const content = useField("text");
-  const author = useField("text");
-  const info = useField("text");
+  const fieldsObj = {
+    content: useField("text", "content"),
+    author: useField("text", "author"),
+    info: useField("text", "info"),
+  };
 
-  const formData = [
-    {
-      name: "content",
-      label: "Content",
-      value: content.value,
-      fnSate: content.onChange,
-    },
-    {
-      name: "author",
-      label: "Author",
-      value: author.value,
-      fnSate: author.onChange,
-    },
-    {
-      name: "info",
-      label: "URL for more info",
-      value: info.value,
-      fnSate: info.onChange,
-    },
-  ];
+  const fieldsArr = Object.values(fieldsObj);
+
+  const data = formData.map((item) => ({
+    ...item,
+    value: fieldsObj[item.name].value,
+    fnSate: fieldsObj[item.name].onChange,
+  }));
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    if (
-      !(
-        content.value.length > 0 &&
-        author.value.length > 0 &&
-        info.value.length > 0
-      )
-    )
-      return;
+    if (fieldsArr.every((field) => field.value.length === 0)) return;
 
     addNew({
-      content: content.value,
-      author: author.value,
-      info: info.value,
+      content: fieldsObj.content.value,
+      author: fieldsObj.author.value,
+      info: fieldsObj.info.value,
       votes: 0,
     });
+  };
+
+  const handleReset = () => {
+    fieldsArr.forEach((field) => field.reset());
   };
 
   return (
@@ -52,7 +39,7 @@ const CreateNew = ({ addNew }) => {
         Create a new Anecdote
       </h2>
       <form onSubmit={handleSubmit}>
-        {formData.map((item) => (
+        {data.map((item) => (
           <div key={item.name} className="flex flex-col gap-2">
             <label className="text-sm font-semibold text-slate-500">
               {item.label}
@@ -66,9 +53,17 @@ const CreateNew = ({ addNew }) => {
           </div>
         ))}
 
-        <button className="min-w-sm px-2 py-2 text-center bg-slate-600 text-slate-100 font-semibold rounded-lg mt-8 hover:bg-slate-700 active:bg-slate-800">
-          Create
-        </button>
+        <div className="flex justify-center items-center mt-4 gap-4">
+          <button className="min-w-3xs px-2 py-2 text-center bg-slate-600 text-slate-100 font-semibold rounded-lg mt-8 hover:bg-slate-700 active:bg-slate-800">
+            Create
+          </button>
+          <button
+            className="min-w-3xs px-2 py-2 text-center bg-gray-200 text-gray-700 font-semibold rounded-lg mt-8 hover:bg-gray-300 active:bg-gray-400"
+            onClick={handleReset}
+          >
+            Reset
+          </button>
+        </div>
       </form>
     </div>
   );
--- part7/routed-anecdotes/src/hooks/useField.jsx
+++ part7/routed-anecdotes/src/hooks/useField.jsx
@@ -1,16 +1,22 @@
 import { useState } from "react";
 
-const useField = (type) => {
+const useField = (type, name) => {
   const [value, setValue] = useState("");
 
   const onChange = (e) => {
     setValue(e.target.value);
   };
 
+  const reset = () => {
+    setValue("");
+  };
+
   return {
+    name,
     type,
     value,
     onChange,
+    reset,
   };
 };
 
